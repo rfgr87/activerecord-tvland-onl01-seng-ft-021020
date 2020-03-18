4c7616d1fafd08b93767c575ee5b557e2a32b12b
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-18 20:36:11 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name
+    fullname = [self.first_name,self.last_name].join(" ")
+    fullname
+  end
+  
+  def list_roles
+    list = [self.characters.first.name, self.characters.first.show.name].join(" - ")
+    list
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters
+  
+  def actors_list
+    self.characters.collect{|x| x.actor.full_name}
+  end
   
 end
