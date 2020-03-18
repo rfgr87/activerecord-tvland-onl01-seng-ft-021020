9acb2e030b80a3c05d83cebb073ece3e376ab68a
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-18 19:42:27 +0000

    Automatically backed up by Learn

--- db/migrate/005_add_character.rb
+++ db/migrate/005_add_character.rb
@@ -1,6 +1,6 @@
 class AddCharacterTable < ActiveRecord::Migration[5.1]
    def change
-    create_table :character do |t|
+    create_table :characters do |t|
       t.string :name
       t.integer :actor_id 
       t.integer :show_id
