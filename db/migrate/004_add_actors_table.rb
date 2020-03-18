df78b3f45e30fbfe55810981157f05cce5101887
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-18 19:38:06 +0000

    Automatically backed up by Learn

--- db/migrate/004_add_actors_table.rb
+++ db/migrate/004_add_actors_table.rb
@@ -1,6 +1,8 @@
 class AddActorsTable < ActiveRecord::Migration[5.1]
   def change
     create_table :actors do |t|
-      t.string , :network_id, :integer
+      t.string :first_name
+      t.string :last_name
+    end 
   end
 end
